a7c26dd8e90ea034475b1253a5d6735db8bc0d87
DilyanaDobreva <dilyana.dobreva@gmail.com>
2019-10-30 16:02:42 +0200

    Working Data Project

--- CocktailMagician/CocktailMagician.Data/Models/Bann.cs
+++ CocktailMagician/CocktailMagician.Data/Models/Bann.cs
@@ -18,12 +18,8 @@ namespace CocktailMagician.Data.Models
         public string Id { get; set; }
         public string Reason { get; set; }
         public DateTime EndDateTime { get; set; }
-<<<<<<< HEAD
-=======
         public bool IsDeleted { get; set; }
->>>>>>> f1c077fc7ca1c09a2a1de7f3378bbabb698838aa
         public string UserId { get; set; }
         public User User { get; set; }
-        public bool IsDeleted { get; set; }
     }
 }
