4cb01969a0ba2ae3821cb48b72d3f9b04fdda685
DilyanaDobreva <dilyana.dobreva@gmail.com>
2019-10-30 15:02:33 +0200

    Warning removed

--- CocktailMagician/CocktailMagician.Services/Contracts/IIngredientServices.cs
+++ CocktailMagician/CocktailMagician.Services/Contracts/IIngredientServices.cs
@@ -1,5 +1,7 @@
 ﻿using CocktailMagician.Data.Models;
+using CocktailMagician.Services.DTOs;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CocktailMagician.Services.Contracts
@@ -10,5 +12,6 @@ namespace CocktailMagician.Services.Contracts
         Task<Ingredient> Get(int id);
         Task Edit(int id, string newName);
         Task Delete(int id);
+        Task<List<IngredientDTO>> GetAllDTO();
     }
 }
