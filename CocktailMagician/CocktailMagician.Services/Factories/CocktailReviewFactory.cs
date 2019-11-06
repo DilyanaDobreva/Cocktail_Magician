5829344a40c941b3e9700ae601aa6e688c74dfb9
Kristian9304 <kristiqntashev@gmail.com>
2019-11-06 21:54:14 +0200

    Adding Cocktail factory

---
+++ CocktailMagician/CocktailMagician.Services/Factories/CocktailReviewFactory.cs
@@ -0,0 +1,24 @@
+﻿using CocktailMagician.Data.Models;
+using CocktailMagician.Services.Contracts.Factories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CocktailMagician.Services.Factories
+{
+    public class CocktailReviewFactory : ICocktailReviewFactory
+    {
+        public CocktailReview Create(string comment, int? rating, User user, Cocktail cocktail)
+        {
+            var review = new CocktailReview
+            {
+                Comment = comment,
+                Rating = rating,
+                User = user,
+                Cocktail = cocktail
+
+            };
+            return review;
+        }
+    }
+}
